4a98cacc01e61eba20d4ad8f048d8b808fb313a6
ceburns1 <ceburns1@loyola.edu>
2024-09-17 17:35:13 -0400

    #Programmers: Caitlin Burns #course: CS151 #due date: 9/18/24 #programming assignment: PA00 #Problem statement: converting kilometers to miles #Data in: number of kilometers #Data out: miles #Credits: Zybooks and class examples

---
+++ main.py
@@ -0,0 +1,9 @@
+#this code converts kilometers to miles
+
+km_str= input ('enter kilometers:')
+km_int= int(km_str)
+
+#convert kilometers into miles by multiplying by .62137
+
+mile= .62137 * km_int
+print("this many miles:" , mile)
